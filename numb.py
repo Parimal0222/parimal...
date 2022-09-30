fc5c6b9a73479a5b29edd4f3f0bfcf22763ac226
Parimal Shende <113876762+Parimal0222@users.noreply.github.com>
2022-09-30 11:06:48 +0530

    Add files via upload

---
+++ numb.py
@@ -0,0 +1,4 @@
+Python 3.6.8 (tags/v3.6.8:3c6b436a57, Dec 23 2018, 23:31:17) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: C:/Users/Home/Desktop/number2.py =================
